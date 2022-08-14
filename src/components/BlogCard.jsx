3869e7edf7a7e8980d292cb8bd7734913bf0fced
zgulen <gulenzubeyir@gmail.com>
2022-08-14 11:26:34 +0300

    private Router done task finished.

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -11,12 +11,20 @@ import { red } from "@mui/material/colors";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import { useFetch } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
 
 export default function BlogCard() {
     const { contactList } = useFetch();
+    const navigate = useNavigate()
+
+    const handleShowData = (id) =>{
+        console.log(id)
+        navigate(`details/${id}`)
+    }
     return (
         <div style={{ display: "flex", flexWrap: "wrap" }}>
             {contactList?.map((item, index) => {
+                
                 return (
                     <Card
                         key={index}
@@ -39,7 +47,29 @@ export default function BlogCard() {
                             image={item.img}
                             alt={item.title}
                         />
-                        <CardContent className="content">
+                        <CardContent
+                            className="content"
+                            sx={{
+                                textOverflow: "ellipsis",
+                                bgcolor: (theme) =>
+                                    theme.palette.mode === "dark"
+                                        ? "#101010"
+                                        : "grey.100",
+                                color: (theme) =>
+                                    theme.palette.mode === "dark"
+                                        ? "grey.300"
+                                        : "grey.800",
+                                border: "1px solid",
+                                borderColor: (theme) =>
+                                    theme.palette.mode === "dark"
+                                        ? "grey.800"
+                                        : "grey.300",
+                                borderRadius: 2,
+                                fontSize: "0.875rem",
+                                fontWeight: "700",
+                            }}
+                            onClick= {()=>handleShowData(item.id)}
+                        >
                             <Typography variant="body2" color="text.secondary">
                                 {item.data}
                             </Typography>
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,21 +1,40 @@
 import React from "react";
-import { Route, Routes, BrowserRouter } from "react-router-dom";
+import {
+    Route,
+    Routes,
+    BrowserRouter,
+    Navigate,
+    Outlet,
+} from "react-router-dom";
 import Register from "../pages/Register";
-import Login from "../pages/Login"
-import DashBoard from "../pages/DashBoard"
-import NavBar from "../components/NavBar"
+import Login from "../pages/Login";
+import DashBoard from "../pages/DashBoard";
+import NavBar from "../components/NavBar";
 import UpdateBlog from "../pages/UpdateBlog";
-
+import Details from "../pages/Details";
+import toast from "react-hot-toast"
 
 const Router = () => {
+    function PrivateRouter() {
+        return sessionStorage.getItem("user") ? (
+            <Outlet />
+        ) : (
+            toast.error("Please Login"),
+            <Navigate to="/login" replace />
+        );
+    }
     return (
         <BrowserRouter>
-                <NavBar/>
+            <NavBar />
             <Routes>
                 <Route path="/" element={<DashBoard />} />
                 <Route path="register" element={<Register />} />
                 <Route path="login" element={<Login />} />
                 <Route path="new" element={<UpdateBlog />} />
+
+                <Route path="/details/:id" element={<PrivateRouter />}>
+                    <Route path="" element={<Details />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
